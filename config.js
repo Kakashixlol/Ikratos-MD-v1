e6ce3444076c98deba893aed64aa56ac2066b134
Kakashixlol <142254758+Kakashixlol@users.noreply.github.com>
2023-10-21 23:56:27 +0300

    Update config.js

--- config.js
+++ config.js
@@ -53,10 +53,10 @@ global.ppulsa = '085736178354' //Nomor SimCard Yang Kamu Pake
 global.psaweria = 'https://saweria.com/XiaoYan021' //Link Saweriamu Kalo Nggada Ketik - aja
 
 /*============== NOMOR ==============*/
-global.nomorbot = '68673045381' //Nomor Bot
-global.nomorown = '923470027813' //Nomor Owner
-global.namebot = 'Ikratos-MD-v1' //Nama Bot
-global.nameown = 'ᴀʜᴍᴀᴅ ᴀʟɪ🇵🇰' // Nama Owner
+global.nomorbot = '201508879130' //Nomor Bot
+global.nomorown = '201033401721' //Nomor Owner
+global.namebot = 'kabo' //Nama Bot
+global.nameown = 'kafrawy' // Nama Owner
 
 
 /*============== CHATGPT ==============*/
